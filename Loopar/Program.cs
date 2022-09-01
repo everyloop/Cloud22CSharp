230318390fa624b81762e7eb5bda1ea6868aab9a
Fredrik Johansson <fredrik@everyloop.com>
2022-09-01 22:29:11 +0200

    Add project Loopar

---
+++ Loopar/Program.cs
@@ -0,0 +1,44 @@
+﻿//int i = 1;
+
+//while (i < 10)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//do
+//{
+//    Console.WriteLine("hej");
+//} while (false);
+
+
+//for (int j = 1; j < 10; j++)
+//{
+//    Console.WriteLine(j);
+//}
+
+for (int i = 1; i <= 3; i++)
+{
+    Print1to9fourTimes();
+}
+
+void Print1to9fourTimes()
+{
+    for (int j = 1; j <= 4; j++)
+    {
+        Print1to9();
+    }
+    Console.WriteLine("----------");
+}
+
+void Print1to9()
+{
+    for (int k = 1; k < 10; k++)
+    {
+        Console.Write(k);
+    }
+    Console.WriteLine();
+}
+
+
+
--- Variabler/Program.cs
+++ Variabler/Program.cs
@@ -1,111 +1,123 @@
-﻿// case sensitive    - gör skillnad på små och stora bokstäver
-// case insensitive  - gör inte skillnad på små och stora bokstäver
+﻿//// case sensitive    - gör skillnad på små och stora bokstäver
+//// case insensitive  - gör inte skillnad på små och stora bokstäver
 
-// Uppercase: MYVARIABLE
-// Lowercase: myvariable
-// Pascalcase: MyVariable
-// Camelcase: myVariableThatIsAwesome,  firstName
-// Snakecase: my_variable
+//// Uppercase: MYVARIABLE
+//// Lowercase: myvariable
+//// Pascalcase: MyVariable
+//// Camelcase: myVariableThatIsAwesome,  firstName
+//// Snakecase: my_variable
 
-// Unsigned
-byte myByte = 8;           // Range 0 -> 2^8 = 256
-ushort myUshort = 16;      // Range 0 -> 2^16 = 65536
-uint myUint = 32;         // Range 0 -> 2^32 = 4 294 967 296
-ulong myUlong = 64;        // Range 0 -> 2^64 = 18 446 744 073 709 551 616
+//// Unsigned
+//byte myByte = 8;           // Range 0 -> 2^8 = 256
+//ushort myUshort = 16;      // Range 0 -> 2^16 = 65536
+//uint myUint = 32;         // Range 0 -> 2^32 = 4 294 967 296
+//ulong myUlong = 64;        // Range 0 -> 2^64 = 18 446 744 073 709 551 616
 
-// Signed
-SByte mySByte = 8;       // Range -128 till +128
-sbyte mySbyte = 8;
+//// Signed
+//SByte mySByte = 8;       // Range -128 till +128
+//sbyte mySbyte = 8;
 
-Int16 myInt16 = 16;      // Range minus 32768 till plus 32767 
-short myShort = 16;
+//Int16 myInt16 = 16;      // Range minus 32768 till plus 32767 
+//short myShort = 16;
 
-Int32 myInt32 = 32;      // Range minus ~2.1 miljarder till plus ~2.1 miljarder
-int myInt = 32;
+//Int32 myInt32 = 32;      // Range minus ~2.1 miljarder till plus ~2.1 miljarder
+//int myInt = 32;
 
-Int64 myInt64 = 64;      // Range 18 446 744 073 709 551 616
-long myLong = 64;
+//Int64 myInt64 = 64;      // Range 18 446 744 073 709 551 616
+//long myLong = 64;
 
-// Datatyper som lagar siffror som inte är heltal
+//// Datatyper som lagar siffror som inte är heltal
 
-Single mySingle = 3.14f;
-float myFloat = 3.14f;          // 32 bitar
+//Single mySingle = 3.14f;
+//float myFloat = 3.14f;          // 32 bitar
 
-Double myDoubleNet = 3.14;
-double myDouble = 3.14;         // 64 bitar
+//Double myDoubleNet = 3.14;
+//double myDouble = 3.14;         // 64 bitar
 
-Decimal myDecimalNet = 3.14M;
-decimal myDecimal = 3.14M;      // 128 bitar
+//Decimal myDecimalNet = 3.14M;
+//decimal myDecimal = 3.14M;      // 128 bitar
 
-// Implict typad variabel (med keyword var)
-var implicitVar = 5.0f;
+//// Implict typad variabel (med keyword var)
+//var implicitVar = 5.0f;
 
-// Suffix
-// f för float (ex: 0.5f)
-// M för decimal (ex: 0.5M)
-// L för long (ex: 5L)
+//// Suffix
+//// f för float (ex: 0.5f)
+//// M för decimal (ex: 0.5M)
+//// L för long (ex: 5L)
 
-String myStringDotNet = "";
-string myString = "använd för att lagra text";
+//String myStringDotNet = "";
+//string myString = "använd för att lagra text";
 
-Char myCharDotNet = 'A';
-char myChar = '5';
+//Char myCharDotNet = 'A';
+//char myChar = '5';
 
-Boolean myBoolDotNet = false;
-bool myBool = true;
+//Boolean myBoolDotNet = false;
+//bool myBool = true;
 
-// Variabler
+//// Variabler
 
-var x = 5.0;
+//var x = 5.0;
 
-x = 5;
+//x = 5;
 
-var y = x + 2;
+//var y = x + 2;
 
-Console.WriteLine("x = " + x + ", y = " + y);
-Console.WriteLine("x = {0}, y = {1}", x, y);
-Console.WriteLine($"x = {x}, y = {y}");
-
-
-string firstName = "Fredrik";
-double radius = 5.3;
+//Console.WriteLine("x = " + x + ", y = " + y);
+//Console.WriteLine("x = {0}, y = {1}", x, y);
+//Console.WriteLine($"x = {x}, y = {y}");
 
 
-// Block Scope: Variabler är endast giltiga i det kodblock de deklarerats i.
+//string firstName = "Fredrik";
+//double radius = 5.3;
 
-myFunction();
 
-if (firstName == "Fredrik")
+//// Block Scope: Variabler är endast giltiga i det kodblock de deklarerats i.
+
+//myFunction();
+
+//if (firstName == "Fredrik")
+//{
+//    string lastName = "Johansson";
+
+//    if (lastName == "Johansson")
+//    {
+//        int age = 41;
+//        Console.WriteLine($"{firstName} {lastName}");
+//    }
+//}
+
+//myFunction();
+//myFunction();
+
+
+//// Method/Function Scope: Varibler är endast giltiga i funktionen/metoden de deklareras i.
+//void myFunction()
+//{
+//    string text = "hejsan";
+//    Console.WriteLine(text);
+//}
+
+//// Console.WriteLine(text); Variablen "text" går inte att använda utanför funktionen
+
+//int inputValue;
+
+//inputValue = Int32.Parse(Console.ReadLine());
+
+////int number = Convert.ToInt32(inputValue);
+////int number = Int32.Parse(inputValue);
+
+//Console.WriteLine(inputValue + 2);
+
+
+Console.Write("Skriv ditt namn: ");
+string name = Console.ReadLine();
+
+if (name == "Fredrik")
 {
-    string lastName = "Johansson";
-
-    if (lastName == "Johansson")
-    {
-        int age = 41;
-        Console.WriteLine($"{firstName} {lastName}");
-    }
+    Console.WriteLine("Du är Fredrik");
 }
-
-myFunction();
-myFunction();
-
-
-// Method/Function Scope: Varibler är endast giltiga i funktionen/metoden de deklareras i.
-void myFunction()
+else
 {
-    string text = "hejsan";
-    Console.WriteLine(text);
+    Console.WriteLine("Vem är du?");
 }
-
-// Console.WriteLine(text); Variablen "text" går inte att använda utanför funktionen
-
-int inputValue;
-
-inputValue = Int32.Parse(Console.ReadLine());
-
-//int number = Convert.ToInt32(inputValue);
-//int number = Int32.Parse(inputValue);
-
-Console.WriteLine(inputValue + 2);
-
 
