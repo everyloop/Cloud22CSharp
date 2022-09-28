c486530aec912b3499dfe1c746f64d9e54f48763
Fredrik Johansson <fredrik@everyloop.com>
2022-09-28 15:39:26 +0200

    Project update Demo: Expression body example

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,103 +1,39 @@
-﻿
-Animal myCat = new Cat() { Name = "Felix" };
-Animal myDog = new Dog() { Name = "Karo" };
-Animal me = new Human() { Name = "Fredrik " };
-SpeakFiveTimes(myCat);
-Console.WriteLine();
-SpeakFiveTimes(myDog);
-Console.WriteLine();
-SpeakFiveTimes(me);
-Console.WriteLine();
-SpeakFiveTimes(5.3f);
+﻿Human me = new Human();
 
+//me.BMI = 25;
 
-void SpeakFiveTimes(object obj)
+double myDouble = me.BMI;
+
+Console.WriteLine(me);
+
+class Human
 {
-    for (int i = 0; i < 5; i++)
-    {
-        /*
-        Animal animal = (Animal)obj;
-        Animal animal = obj as Animal;
-
-        if (animal is not null)
-        {
-            animal.Speak();
-        }
-        */
-
-        if (obj is Animal animal)
-        {
-            //Animal animal = (Animal)obj;
-            animal.Speak();
-        }
-        else if (obj is float f)
-        {
-            Console.WriteLine($"Float value: {f}");
-        }
+    private double _BMI = 25.0;
+    /*
+    public double BMI 
+    { 
+        get { return _BMI; } 
+        //set { _BMI = value; }
     }
-}
-
-abstract class Animal
-{
-    public static int Age { get; set; }
-
-    public virtual string Name { get; set; }
-
-    public abstract void Speak();
-
-}
-
-class Cat : Animal
-{
-    public static int Age { get; set; }
-
-    public override void Speak()
+    */
+    /*  Expression Body
+    public double BMI
     {
-        Console.WriteLine("I'm a cat!");
+        get => _BMI;
+        set => _BMI = value;
     }
+    */
+
+    public double BMI => _BMI;
+
 
     public override string ToString()
     {
-        return $"My name is {Name}, and I like milk.";
+        return $"I'm a human with BMI: {_BMI}";
     }
 
-}
+    // Method with expression body syntax:
+    //public override string ToString() => $"I'm a human with BMI: {_BMI}";
 
-class Dog : Animal
-{
-    public static int Age { get; set; }
-
-    public override void Speak()
-    {
-        Console.WriteLine("I'm a dog!");
-    }
-}
-
-class Human : Animal
-{
-    public static int Age { get; set; }
-
-    private string _name;
-
-    public float MaxHealth
-    {
-        get { return 12.0f; }
-    }
-
-    public override string Name
-    {
-        get => $"*** {_name} ***";
-        set => _name = value;
-    }
-
-    public override void Speak()
-    {
-        Console.WriteLine("I'm a human!");
-    }
-
-    public override string ToString()
-    {
-        return $"My name is {Name}, and I am a human.";
-    }
 
 }
