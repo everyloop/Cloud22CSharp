05f64a3c51d7a70b607b7a9a655f9535d45a2d17
Fredrik Johansson <fredrik@everyloop.com>
2022-10-01 10:31:41 +0200

    Add projects: Generics Interface

---
+++ Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿PrintFiveTimes("Hello");
+Console.WriteLine();
+PrintFiveTimes(true);
+Console.WriteLine();
+
+Cage<string> stringCage = new("Hello");
+
+Cage<int> intCage = new(3);
+intCage.Inhabitant = 5;
+
+DoubleCage<string, bool> doubleCage = new("hej", true);
+doubleCage.InhabitantB = false;
+
+Console.WriteLine(stringCage.Inhabitant);
+Console.WriteLine(intCage.Inhabitant);
+Console.WriteLine(doubleCage.InhabitantA);
+Console.WriteLine(doubleCage.InhabitantB);
+
+void PrintFiveTimes<T>(T item)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		Console.WriteLine(item);
+	}
+}
+
+class Cage<T>
+{
+	public T Inhabitant { get; set; }
+
+	public Cage(T item)
+	{
+		Inhabitant = item;
+	}
+}
+
+class DoubleCage<T, T2>
+{
+	public T InhabitantA { get; set; }
+	public T2 InhabitantB { get; set; }
+
+	public DoubleCage(T a, T2 b)
+	{
+		InhabitantA = a;
+		InhabitantB = b;
+	}
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+using (var myCat = new Cat("Felix", 3.1))
+{
+	Console.WriteLine($"The name of the cat is: {myCat.Name}");
+}
+
+Console.WriteLine();
+
+Cat[] cats = {
+	new Cat("Felix", 3.4),
+	new Cat("Findus", 1.4),
+	new Cat("Misse", 2.8),
+	new Cat("Kisse", 2.7),
+	new Cat("Pisse", 1.9),
+};
+
+Array.Sort(cats);
+
+foreach (Cat cat in cats)
+{
+	Console.WriteLine($"{cat.Name} weights {cat.Weight} kg.");
+	if (cat is IExplodable explodable)
+	{
+		explodable.Explode();
+	}
+
+}
+
+class Animal : IExplodable
+{
+	public double ExplosionVelocity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+	public string Name { get; set; }
+
+	public void Explode()
+	{
+		Console.WriteLine($"{Name} explodes!");
+	}
+}
+
+class Cat : Animal, IDisposable, IComparable<Cat>
+{
+	public double Weight { get; set; }
+
+	public Cat(string name, double weight)
+	{
+		Name = name;
+		Weight = weight;
+	}
+
+	public void Dispose()
+	{
+		Console.WriteLine($"{Name} is disposed!");
+	}
+
+	public int CompareTo(Cat cat)
+	{
+		//return Name.CompareTo((obj as Cat).Name);
+		return Weight.CompareTo(cat.Weight);
+	}
+}
+
+interface IExplodable
+{
+	public double ExplosionVelocity { get; set; }
+	public void Explode();
+}
